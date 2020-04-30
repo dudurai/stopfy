4cf00e048243de8b11293836d8e32673a65abcd0
EduhDev <eduardocarvalho970@gmail.com>
2020-04-29 23:10:56 -0300

    corrige problema com build blank screen

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,21 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
-import reducers from "./ducks";
+import "../config/reactotron";
+
 import sagas from "./sagas";
+import reducers from "./ducks";
 
 const sagaMonitor =
   process.env.NODE_ENV === "development"
     ? console.tron.createSagaMonitor()
     : null;
-const sagaMiddleware = createSagaMiddleware(sagaMonitor);
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 const middlewares = [sagaMiddleware];
 
 const composer =
   process.env.NODE_ENV === "development"
-    ? compose(
-        applyMiddleware(...middlewares),
-        console.tron.createEnhancer()
-      )
+    ? compose(applyMiddleware(...middlewares), console.tron.createEnhancer())
     : applyMiddleware(...middlewares);
 
 const store = createStore(reducers, composer);
